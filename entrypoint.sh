bb0727cf6e188575b1ad18f1421e8e4198e64435
Thanatat Tamtan <acoshift@gmail.com>
2020-01-16 21:15:16 +0700

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ chmod 400 ./ssh_key
 
 ssh -i ./ssh_key "${TUNNEL_USER}@${TUNNEL_IP}" \
     -N -D 127.0.0.1:5000 \
-    -o StrictHostKeyChecking=no
+    -o StrictHostKeyChecking=no &
 
 export HTTP_PROXY=socks5://127.0.0.1:5000
 
